c3e59a5b67c5fb8c39afbbbd3b983a2f0f11d34c
Yannis.A <33091253+yannisandre@users.noreply.github.com>
2024-11-24 14:15:18 +0100

    Delete database.ts

--- database.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const database = new Sequelize('LearningFactDb', 'learningdbuser', 'mdp123', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: console.log,
-});
-
-export default database;
